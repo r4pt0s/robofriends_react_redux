a642fcbd761ecaa64d168310e5c06357506fbee6
r4pt0s <r4pt0s@protonmail.com>
2018-11-08 21:43:46 +0100

    Updates

---
+++ precache-manifest.8a623219c0b5209a3c0737d62215f72b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/robofriends_react_redux/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "8758ee32e26d0f11012f",
+    "url": "/robofriends_react_redux/static/js/runtime~main.8758ee32.js"
+  },
+  {
+    "revision": "f55d558db78b751075fa",
+    "url": "/robofriends_react_redux/static/js/main.f55d558d.chunk.js"
+  },
+  {
+    "revision": "63ad911744b5409c8cd8",
+    "url": "/robofriends_react_redux/static/js/1.63ad9117.chunk.js"
+  },
+  {
+    "revision": "f55d558db78b751075fa",
+    "url": "/robofriends_react_redux/static/css/main.16a8d6d0.chunk.css"
+  },
+  {
+    "revision": "63ad911744b5409c8cd8",
+    "url": "/robofriends_react_redux/static/css/1.c5632152.chunk.css"
+  },
+  {
+    "revision": "145e1bdb45d825259c803a26aacaf396",
+    "url": "/robofriends_react_redux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/robofriends_react_redux/precache-manifest.8a623219c0b5209a3c0737d62215f72b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/robofriends_react_redux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
